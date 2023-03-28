5be672df5f94b60b59bbd4b28485c157e99a2335
Frontend JavaScript <frontendjavascript@C3-16.local>
2023-03-28 20:43:04 +0300

    curs 2 oop

--- curs1/js/Carte.js
+++ curs1/js/Carte.js
@@ -1,10 +1,23 @@
 class Carte {
-    constructor(titlu,autor,editura,an,pret) {
+    static reducere = 10;
+    constructor(titlu,autor,editura,an,pret,nrPagini) {
         this.titlu = titlu;
         this.autor = autor;
         this.editura = editura;
         this.an = an;
         this.pret =pret;
+        this.nrPagini = nrPagini;
+    }
+    get pret (){
+        console.log('sunt in getter')
+        return this.pret;
+    }
+    set pret (pretNou){
+        console.log('sunt in setter')
+        this.pret = pretNou;
+    }
+    info(){
+        console.log(`Titlul este ${this.titlu},autor ${this.autor}`);
     }
     deschidCartea(pagina){
         console.log(`Am deschis cartea la pagina ${pagina}`);
@@ -22,6 +35,12 @@ class Carte {
       // this.pret = this.pret - (procent/100* this.pret)
        this.pret -=(procent * this.pret)/100
        console.log( `pretul cu reducere este ${this.pret}`);
+   }
+   pretNou(){
+    this.pret = this.pret - (this.reducere * this.pret) / 100;
+   }
+   modificaPret(diferenta){
+     this.pret = this.pret - diferenta;
    }
      
 
