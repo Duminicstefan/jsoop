ff038a5d847d52ef51a99b9449d199592fc6ea3a
Frontend JavaScript <frontendjavascript@C3-16.local>
2023-03-30 20:24:41 +0300

    curs3

---
+++ curs2/solutii/js/app3.js
@@ -0,0 +1,4 @@
+import {Caine} from '../modules/Caine.js';
+
+const myJavra =new Caine(6,4,'uzi');
+console.log(myJavra);
--- curs2/solutii/modules/User.js
+++ curs2/solutii/modules/User.js
@@ -1,12 +1,47 @@
-class User{
-    constructor(id,nume,email, varsa){
+export class User{
+     _varsta; //protected
+     #email; //private
+    constructor(id,nume,email, varsta){
         //setam proprietatile
+        this.id = id;
+        this.nume = nume;
+        this.email = email;
+        this.varsta = varsta;
     }
-    mananca (claorii){
-        console.log(`Mananca ${claorii} pe zi.`);
+    get email(){
+        console.log('sunt in getter email')
+        return this.#email
+       
+    }
+    set email(newEmail){
+        console.log('sunt in setter email')
+        if(newEmail == 'dan@dan.com'){
+
+        this.#email = newEmail;
+        }
+        else{
+            throw 'error invalid data'
+        }
+
+    }
+    get myAge(){
+        return this._varsta;
+    }
+    set myAge(varstaNoua){
+        if(this.varstaNoua >0){
+            this._varsta = varstaNoua
+        }
+        else{
+            throw 'error invalid data'
+            // console.error('Error')
+        }
+
+    }
+    mananca (calorii){
+        console.log(`Mananca ${calorii}  calorii pe zi.`);
     }
     doarme(ore){
-        console.log(`Doarme ${ore} pe zi`);
+        console.log(`Doarme ${ore}  ore pe zi`);
     }
 }
-export {User}
+// export {User}
